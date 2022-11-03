6984941b616aa33c4d03164820651ee9e6ecab97
777shuang <shuang777@outlook.jp>
2022-11-03 16:49:21 +0900

    httpclientからpuppyに変更

--- main.nim
+++ main.nim
@@ -1,10 +1,11 @@
-import httpclient
+import puppy
 import strutils
 
 const url = "https://777shuang.web.fc2.com/apt-brain/list.csv"
-echo("Connecting to " + url + " ...")
-let client = newHttpClient()
-let response = client.get(url)
-let lines = split(response.body , "\n\r")
+echo("Connecting to \"" & url & "\" ...")
+let req = Request(url: parseUrl(url) , verb: "get")
+let res = fetch(req)
+let lines = split(res.body , "\n\r")
 for line in lines:
   let tmp = split(line , ",")
+  echo(tmp[0])
