727d0231af2699d9df0a014c6f79079632566558
777shuang <shuang777@outlook.jp>
2022-11-03 17:12:26 +0900

    Update

--- main.nim
+++ main.nim
@@ -1,11 +1,25 @@
 import puppy
 import strutils
+import os
 
-const url = "https://raw.githubusercontent.com/GenkaiSoft/apt-brain/main/list.csv"
-echo("Connecting to \"" & url & "\" ...")
-let req = Request(url: parseUrl(url) , verb: "get")
-let res = fetch(req)
-let lines = split(res.body , "\n\r")
-for line in lines:
-  let tmp = split(line , ",")
-  echo(tmp[0])
+if paramCount() == 0:
+  echo(
+    """
+     ____            _                         _    ∧  ∧
+    | __ ) _ __ __ _(_)_ __   ___ ___  _ __ __| | ／￣￣￣＼
+    |  _ \| '__/ _` | | '_ \ / __/ _ \| '__/ _` | | ＊  ＊ |
+    | |_) | | | (_| | | | | | (_| (_) | | | (_| | |＝ __ ＝|
+    |____/|_|  \__,_|_|_| |_|\___\___/|_|  \__,_| ＼______／
+    """
+  )
+elif paramCount() == 2 and commandLineParams()[0] == "install":
+  const url = "https://raw.githubusercontent.com/GenkaiSoft/apt-brain/main/list.csv"
+  echo("Connecting to \"" & url & "\" ...")
+  let req = Request(url: parseUrl(url) , verb: "get")
+  let res = fetch(req)
+  let lines = split(res.body , "\n\r")
+  for line in lines:
+    let tmp = split(line , ",")
+    if tmp[0] == commandLineParams()[1]:
+      echo(tmp[0])
+      break
