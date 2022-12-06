f8d0e596c0e389e1fcea8af28ffd8bc8772bc880
777shuang <shuang777@outlook.jp>
2022-12-06 15:19:06 +0900

    Updating

--- src/cmd/install/zip.nim
+++ src/cmd/install/zip.nim
@@ -2,28 +2,22 @@ import std/[strutils , streams , os]
 import zippy/ziparchives
 import ../../common
 
-proc installZip*(listUrl , zipFileName:string) =
+proc installZip*(listUrl , zipFileName:string) {.inline.} =
   let list = connect(listUrl)
   if list != "":
     let reader = openZipArchive(zipFileName)
-    for files in split(list , "\n"):
-      for path in split(files , ","):
-        var dir:string = appDir
-        for subDir in split(path , "/"):
-          dir = dir / subDir
-          createDir(dir)
-          let fileName = dir / path.substr(path.rfind("/") + 1)
-          showProcess("Opening file \"" & fileName & "\"")
-          var strm:Stream
-          try:
-            strm = openFileStream(fileName)
-          except IOError:
-            showFailed()
-            showExc("Unable to open file \"" & fileName & "\"")
-          showDone()
-          showProcess("Writing file \"" & fileName & "\"")
-          try:
-            strm.write(reader.extractFile(path))
-          except ZippyError:
-            showFailed()
-            showExc("Unable to extract file \"" & path[0] & "\"")
+    for line in split(list , "\n"):
+      let
+        path = split(line , ",")
+        inputFileName = appDir / path[0]
+        outputFileName = appDir / path[1]
+      try:
+        showProcess("Opening file \"" & inputFileName & "\"")
+        let strm = openFileStream(inputFileName)
+        showDone()
+        showProcess("Writing file \"" & outputFileName & "\"")
+        strm.write(reader.extractFile(path[0]))
+        strm.close()
+      except IOError:
+        showExc("Unable to open file \"" & inputFileName & "\"")
+      except ZippyError:
