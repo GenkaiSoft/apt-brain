b7cd255c086a3a303ee5c1861383fdfe2cab8e1c
777shuang <shuang777@outlook.jp>
2022-12-09 08:10:01 +0900

    Updating

--- src/cmd/show.nim
+++ src/cmd/show.nim
@@ -2,20 +2,14 @@ import std/strutils
 import ../common
 
 proc cmdShow*() {.inline.} =
-  let res = connect(url)
-  if res != "":
-    showLog("Show package(s)")
-    let lines = split(res , "\n")
-    var exist = false
-    if cmdLineParamCount == 1:
-      for line in lines:
-        showInfo(split(line , ",")[0])
-    else:
-      for line in lines:
-        let value = split(line , ",")[0]
-        if value.toLower == cmdLineParams[1].toLower:
-          showInfo("Package \"" & value & "\" exists.")
-          exist = true
-          break
-    if not exist and cmdLineParamCount != 1:
+  if cmdLineParamCount == 1:
+    for key in fields.keys:
+      showInfo(key)
+  else:
+    for key in fields.keys:
+      if key.toLower == cmdLineParams[1].toLower:
+        showInfo("Package \"" & key & "\" exists.")
+        exist = true
+        break
+    if not exist:
       showErr("Package \"" & cmdLineParams[1] & "\" does not exist")
--- src/common.nim
+++ src/common.nim
@@ -2,13 +2,7 @@ import std/[strutils , os , terminal]
 import puppy
 import time/ctime
 
-const
-  url* = "https://raw.githubusercontent.com/GenkaiSoft/apt-brain/main/list.csv"
-  appDir* = "アプリ"
-let
-  cmdLineParamCount* = paramCount()
-  cmdLineParams* = commandLineParams()
-
+proc quote(str:string):string = return " \"" & str & "\""
 proc show(f:File , msgType , str:string , color:ForegroundColor) =
   f.styledWrite(
     fgMagenta ,
@@ -29,14 +23,14 @@ proc show(f:File , msgType , str:string , color:ForegroundColor) =
   f.styledWrite(fgWhite , " > ")
   f.styledWrite(color , str)
   f.write("\n")
-proc showErr*(str:string) = stderr.show("Error      " , str & " !" , fgRed)
+proc showErr*(str:string) = stderr.show("Error      " , str & " !\a" , fgRed)
 proc showLog*(str:string) = stdout.show("Log        " , str , fgGreen)
 proc showInfo*(str:string) = stdout.show("Information" , str , fgBlue)
 proc showWarn*(str:string) = stdout.show("Warning    " , str & " !" , fgYellow)
 proc showDbg*(str:string) = stdout.show("Debug      " , str , fgWhite)
 proc showExc*(str:string) =
   showErr(str)
-  showErr("Error message is \"" & getCurrentExceptionMsg() & "\"")
+  showErr("Error message is" & quote(getCurrentExceptionMsg()))
 proc showProcess*(str:string) = showLog(str & " ...")
 
 proc showResult(str:string) = showLog("-> " & str)
@@ -48,12 +42,21 @@ proc showFew*() = showArgs("few")
 proc showMany*() = showArgs("many")
 
 proc connect*(url:string):string =
-  showProcess("Connecting to \"" & url & "\" ...")
+  showProcess("Connecting to" & quote(url))
   let res = fetch(Request(url: parseUrl(url) , verb: "get"))
   if res.code == 200:
     showDone()
   else:
     showFailed()
-    showErr("Unable to connect to \"" & url & "\"")
+    let msg = "Unable to connect to" & quote(url)
+    showErr(msg)
+    raise newException(OSError , msg)
   showLog("Response code is \'" & res.code.intToStr & "\'")
   return res.body
+
+const
+  url* = "https://raw.githubusercontent.com/GenkaiSoft/apt-brain/main/list.csv"
+  appDir* = "アプリ"
+let
+  cmdLineParamCount* = paramCount()
+  cmdLineParams* = commandLineParams()
