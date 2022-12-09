1e2bc6168f3b5fe80164b47214ad3911653d8ab3
777shuang <shuang777@outlook.jp>
2022-12-09 21:32:48 +0900

    Updating

--- src/cmd/help.nim
+++ src/cmd/help.nim
@@ -7,22 +7,23 @@ proc cmdHelp*() {.inline.} =
   const helps = {
     "help":[["help" , "Show help"] , ["help {command(s)}" , "Show help about {command(s)}."]] ,
     "install":[
-      ["install {package(s)}" , "Install {package(s)} to \"{current directory}" / "アプリ" / "{{package} name}\"."] ,
+      ["install {package(s)}" , "Install {package(s)} to \"{current directory}" / appDir / "{{package} name}\"."] ,
       ["install [--dir|-d] {directory} {package(s)}" , "Install {packages(s)} to {directory}."]
     ] ,
     "ping":[["ping" , "Measure ping."] , null] ,
     "version":[["version" , "Show version."] , null] ,
     "show":[["show" , "Show all packages"] , ["show [package]" , "Show about [package]."]]
   }.toTable
+  const space = " : "
   if cmdLineParamCount == 1:
     for key , help in helps:
       for value in help:
         if value != null:
-          showInfo("\"" & value[0] & "\" : " & value[1])
+          showInfo(value[0].quote & space & value[1])
   else:
     for key , help in helps:
       if key == cmdLineParams[1]:
         for value in help:
           if value != null:
-            showInfo("\"" & value[0] & "\" : " & value[1])
+            showInfo(value[0].quote & space & value[1])
         break
--- src/common.nim
+++ src/common.nim
@@ -3,27 +3,28 @@ import puppy
 import time/ctime
 
 const
-  url* = "https://raw.githubusercontent.com/GenkaiSoft/apt-brain/main/list.csv"
+  jsonUrl* = "https://raw.githubusercontent.com/GenkaiSoft/apt-brain/main/package.json"
   appDir* = "アプリ"
 let
   cmdLineParamCount* = paramCount()
   cmdLineParams* = commandLineParams()
 
-proc quote(str:string):string = return " \"" & str & "\""
+proc quote(str:string):string = return " \"" & str & "\" "
 proc show(f:File , msgType , str:string , color:ForegroundColor) =
   f.styledWrite(
     fgMagenta ,
-    "[" &
-    getYear().intToStr &
-    "/" &
-    getMonth().intToStr &
-    "/" &
-    getDay().intToStr &
-    " " &
-    getHour().intToStr &
-    ":" &
-    getMinute().intToStr &
-    ":" & getSecond().intToStr &
+    "[",
+    getYear().intToStr,
+    "/",
+    getMonth().intToStr,
+    "/",
+    getDay().intToStr,
+    " ",
+    getHour().intToStr,
+    ":",
+    getMinute().intToStr,
+    ":",
+    getSecond().intToStr,
     "] "
   )
   f.styledWrite(color , msgType)
@@ -38,28 +39,36 @@ proc showDbg*(str:string) = stdout.show("Debug      " , str , fgWhite)
 proc showExc*(str:string) =
   showErr(str)
   showErr("Error message is" & quote(getCurrentExceptionMsg()))
-proc showProcess*(str:string) = showLog(str & " ...")
 
 proc showResult(str:string) = showLog("-> " & str)
 proc showFailed*() = showResult("failed")
 proc showDone*() = showResult("done")
+
+proc showProcess*(str:string) = showLog(str & " ...")
+proc showProcess*(str:string , p:proc():string) =
+  showProcess(str)
+  let msg = p()
+  if msg == "":
+    showDone()
+  else:
+    showFailed()
+    showErr(msg)
 
 proc showArgs(str:string) = showErr("Too " & str & " arguments")
 proc showFew*() = showArgs("few")
 proc showMany*() = showArgs("many")
 
 proc connect*(url:string):string =
-  showProcess("Connecting to" & quote(url))
-  let res = fetch(Request(url: parseUrl(url) , verb: "get"))
+  showProcess("Connecting to" & url.quote)
+  let res = fetch(Request(url:parseUrl(url) , verb:"GET"))
   if res.code == 200:
     showDone()
   else:
     showFailed()
-    let msg = "Unable to connect to" & quote(url)
-    showErr(msg)
-    raise newException(OSError , msg)
+    showErr("Unable to connect to " & url.quote)
+    showInfo("Would you use \"https://web.archive.org\" ?")
   showLog("Response code is \'" & res.code.intToStr & "\'")
   return res.body
 
 proc getJson():JsonObj =
-  return pauseJson(connect(url))
+  return pauseJson(connect(jsonUrl))
