58dedf4f1825d28897a0f60037bab275b195db85
777shuang <shuang777@outlook.jp>
2023-02-18 17:12:37 +0900

    Fixed

--- apt_brain.nimble
+++ apt_brain.nimble
@@ -19,7 +19,7 @@ skipFiles = @["package.json"]
 # Tasks
 task build_win , "Cross compile for windows":
   exec "nimble build -d:release -d:mingw --cpu:amd64"
-  exec "zip x64 apt-brain.exe"
+  exec "zip apt-brain_windows_x64 apt-brain.exe"
   exec "nimble build -d:release -d:mingw --cpu:i386"
-  exec "zip x86 apt-brain.exe"
+  exec "zip apt-brain_windows_x86 apt-brain.exe"
   exec "rm apt-brain.exe"
--- src/cmd/help.nim
+++ src/cmd/help.nim
@@ -4,25 +4,26 @@ import std/[os , tables , strutils]
 proc cmdHelp*() {.inline.} =
   const null = ["" , ""]
   const helps = {
-    "help":[["help" , "Show help"] , ["help {command(s)}" , "Show help about {command(s)}."]] ,
+    "help":[["" , "Show help"] , ["{command(s)}" , "Show help about {command(s)}."]] ,
     "install":[
-      ["install {package(s)}" , "Install {package(s)} to \"{current directory}" / appDir / "{{package} name}\"."] ,
-      ["install [--dir|-d] {directory} {package(s)}" , "Install {packages(s)} to {directory}."]
+      ["{package(s)}" , "Install {package(s)} to \"{current directory}" / appDir / "{{package} name}\"."] ,
+      ["[--dir|-d] {directory} {package(s)}" , "Install {packages(s)} to {directory}."]
     ] ,
-    "ping":[["ping" , "Measure ping."] , null] ,
-    "version":[["version" , "Show version."] , null] ,
-    "show":[["show" , "Show all packages"] , ["show [package]" , "Show about [package]."]]
+    "ping":[["" , "Measure ping."] , null] ,
+    "version":[["" , "Show version."] , null] ,
+    "show":[["" , "Show all packages"] , ["show [package]" , "Show about [package]."]] ,
+    "download":[["" , "download package file to current directory"] , ["[--dir|-d] {directory}" , "download package file to {directory}"]]
   }.toTable
   const space = " : "
   if paramCount() == 1:
     for key , help in helps:
       for value in help:
         if value != null:
-          showInfo(value[0].quote & space & value[1])
+          showInfo(quote(key & " " & value[0]) & space & value[1])
   else:
     for key , help in helps:
       if key == commandLineParams()[1].toLower:
         for value in help:
           if value != null:
-            showInfo(value[0].quote & space & value[1])
+            showInfo(quote(key & " " & value[0]) & space & value[1])
         break
