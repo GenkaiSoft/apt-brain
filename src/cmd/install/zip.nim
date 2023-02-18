b7f594d59c18d0bb9ead533461eec9b24e9c39d7
777shuang <shuang777@outlook.jp>
2023-02-18 12:24:33 +0900

    Update

--- src/cmd/install/zip.nim
+++ src/cmd/install/zip.nim
@@ -1,13 +1,24 @@
 import std/[strutils , streams , os]
-import zippy
+import zippy/ziparchives
 import ../../common
 
 proc installZip*(package:Package) {.inline.} =
   let fileName = package.url.substr(package.url.rfind("/") + 1)
-  var strm:Stream
+  var strm :Stream
   try:
     strm = openFileStream(fileName)
   except OSError:
     showExc("Unable to open file" & fileName.quote)
   strm.write(package.url.connect) 
   strm.close()
+  try:
+    let reader = openZipArchive(fileName)
+    for file in 0..package.input.len:
+      try:
+        strm = openFileStream(package.output[file])
+      except OSError:
+        showExc("Failed to open file" & package.output[file].quote)
+      strm.write(reader.extractFile(package.output[file]))
+      strm.close()
+  except ZippyError:
+    showExc("zipfile" & fileName.quote & "is broken")
