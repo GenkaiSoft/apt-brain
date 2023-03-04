9176bfd9f2777d5abbcdebad38fde657f4aa91d7
777shuang <shuang777@outlook.jp>
2023-03-04 22:34:48 +0900

    Update

--- src/apt_brain/cmd/ver.nim
+++ src/apt_brain/cmd/ver.nim
@@ -2,9 +2,9 @@ import liblim/logging
 import ../common
 
 proc cmdVer*() =
-  const NimblePkgVersion {.strdefine.}:string = "0.0.0(DEBUG_BUILD)"
+  const NimblePkgVersion {.strdefine.} = "0.0.0(DEBUG_BUILD)"
   when not defined(release) or not defined(NimblePkgVersion):
     printWarn("It's not release build!")
   printInfo(appName & " v" & NimblePkgVersion)
   printInfo("https://github.com/GenkaiSoft/" & appName)
-  printInfo("Copylight (c) 2022 777shuang. All Rights Reserved.")
+  printInfo("Copylight (c) 2022-2023 777shuang. All Rights Reserved.")
